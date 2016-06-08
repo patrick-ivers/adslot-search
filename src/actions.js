32db16f0b8437accefa2456e7fc3ddaff4ccfd1b
Patrick Ivers <patrick.john.ivers@gmail.com>
2016-06-08 15:11:20 +1000

    Added action creators

---
+++ src/actions.js
@@ -0,0 +1,13 @@
+
+export function setQuery(query) {
+    return {
+        type: 'SET_QUERY',
+        query: query
+    };
+}
+
+export function search() {
+    return {
+        type: 'SEARCH'
+    };
+}
