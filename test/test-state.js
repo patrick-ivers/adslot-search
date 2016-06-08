cf75d03e2d2a5fe1fad523d34d7cb119bec77d88
Patrick Ivers <patrick.john.ivers@gmail.com>
2016-06-08 15:26:49 +1000

    Changed double quotes to single for consistency

--- test/test-state.js
+++ test/test-state.js
@@ -4,38 +4,38 @@ export const TEST_STATE = Immutable.from({
     sites: [
         {
             id: 1,
-            siteName: "React",
-            siteUrl: "facebook.github.io/react",
-            description: "A JavaScript library for building user interfaces",
+            siteName: 'React',
+            siteUrl: 'facebook.github.io/react',
+            description: 'A JavaScript library for building user interfaces',
             categoryIds: [ 2, 3 ]
         },
         {
             id: 2,
-            siteName: "Angular 2",
-            siteUrl: "angular.io",
-            description: "One framework. Mobile and desktop.",
+            siteName: 'Angular 2',
+            siteUrl: 'angular.io',
+            description: 'One framework. Mobile and desktop.',
             categoryIds: [ 1, 2, 3 ]
         },
         {
             id: 3,
-            siteName: "the_coding_love();",
-            siteUrl: "thecodinglove.com",
-            description: "/* epic gifs for developers */",
+            siteName: 'the_coding_love();',
+            siteUrl: 'thecodinglove.com',
+            description: '/* epic gifs for developers */',
             categoryIds: [ 3, 4 ]
         },
         {
             id: 4,
-            siteName: "Reaction GIFS",
-            siteUrl: "reactiongifs.com",
-            description: "Say it with a GIF!",
+            siteName: 'Reaction GIFS',
+            siteUrl: 'reactiongifs.com',
+            description: 'Say it with a GIF!',
             categoryIds: [ 4 ]
         }
     ],
     categories: [
-        { id: 1, description: "Model View Controller" },
-        { id: 2, description: "JavaScript" },
-        { id: 3, description: "Coding" },
-        { id: 4, description: "Humour" }
+        { id: 1, description: 'Model View Controller' },
+        { id: 2, description: 'JavaScript' },
+        { id: 3, description: 'Coding' },
+        { id: 4, description: 'Humour' }
     ],
     search: {
         query: '',
