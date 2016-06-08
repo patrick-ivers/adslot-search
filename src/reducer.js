db5d2256d4ecb8691d929630201273d6e0f47cf7
Patrick Ivers <patrick.john.ivers@gmail.com>
2016-06-08 15:09:16 +1000

    Implemented reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,12 @@
+import { setQuery, search } from './core';
+
 export default function(state, action) {
+    switch (action.type) {
+        case 'SET_QUERY':
+            return setQuery(state, action.query);
+        case 'SEARCH':
+            return search(state);
+    }
+
     return state;
 }
