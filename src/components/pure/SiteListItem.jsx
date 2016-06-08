8468e6f8bdc1a3422616cd35147d47419f0b3b72
Patrick Ivers <patrick.john.ivers@gmail.com>
2016-06-08 16:27:42 +1000

    Forgot to 'return' SiteListItem. Added

--- src/components/pure/SiteListItem.jsx
+++ src/components/pure/SiteListItem.jsx
@@ -1,16 +1,18 @@
 import React, { PropTypes } from 'react';
 
 const SiteListItem = ({ id, url, description }) => {
-    <li key={id} className="SiteListItem">
-        <a className="SiteListItemLink" href="http://{url}" target="_blank">{url}</a>
-        <p className="SiteListItemDesc">{description}</p>
-    </li>
+    return (
+        <li key={id} className="SiteListItem">
+            <a className="SiteListItemLink" href="http://{url}" target="_blank">{url}</a>
+            <p className="SiteListItemDesc">{description}</p>
+        </li>
+    );
 };
 
 SiteListItem.propTypes = {
     id: PropTypes.number.isRequired,
     url: PropTypes.string.isRequired,
-    description: PropTypes.string
+    description: PropTypes.string.isRequired
 };
 
 export default SiteListItem;
