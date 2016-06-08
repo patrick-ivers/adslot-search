f578ab10b4e0826846a3ea96da728ca31ea61990
Patrick Ivers <patrick.john.ivers@gmail.com>
2016-06-08 15:26:08 +1000

    Added initial state to reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,7 @@
+import INITIAL_STATE from './initial-state';
 import { setQuery, search } from './core';
 
-export default function(state, action) {
+export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
         case 'SET_QUERY':
             return setQuery(state, action.query);
