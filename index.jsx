9f8420a4d50584f4d97fb7e71dd37b0c58f777f6
Patrick Ivers <patrick.john.ivers@gmail.com>
2016-06-09 12:46:42 +1000

    Added entry point for app

---
+++ index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './src/components/App';
+import { SearchContainer } from './src/components/Search';
+import reducer from './src/reducer';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App>
+            <SearchContainer />
+        </App>
+    </Provider>
+);
