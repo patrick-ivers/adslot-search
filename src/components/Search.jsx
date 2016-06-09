c36b6e0750be3848a991f543d0c990a5e872f85b
Patrick Ivers <patrick.john.ivers@gmail.com>
2016-06-09 12:36:13 +1000

    Added Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import SearchInput from './pure/SearchInput';
+import SearchResults from './pure/SearchResults';
+
+class Search extends Component {
+
+    render() {
+        const {
+            query,
+            setQuery,
+            search,
+            results
+        } = this.props;
+
+        return (
+            <div className="Search">
+                <SearchInput query={query} setQuery={setQuery} search={search} />
+                <SearchResults results={results} />
+            </div>
+        );
+    }
+
+}
+
+Search.propTypes = {
+    query: PropTypes.string.isRequired,
+    setQuery: PropTypes.func.isRequired,
+    search: PropTypes.func.isRequired,
+    results: PropTypes.array.isRequired
+};
+
+const getResults = (state) => {
+    return state.sites.filter((site) => {
+        return state.search.results.includes(site.id);
+    });
+};
+
+function mapStateToProps(state) {
+    return {
+        query: state.search.query,
+        results: getResults(state)
+    };
+}
+
+export const SearchContainer = connect(
+    mapStateToProps,
+    actions
+)(Search);
